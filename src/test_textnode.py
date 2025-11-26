2ccd3c1f0afb21a92e50b91f93454e38b738b6f7
e-zs <215135216+e-zs@users.noreply.github.com>
2025-11-26 21:58:52 +0100

    added textnode tests

---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+#python
+
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is a text node', TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode('This is a text node', TextType.BOLD)
+        node2 = TextNode('This is another text node', TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_not_eq_text_type(self):
+        node = TextNode('This is a text node', TextType.ITALIC)
+        node2 = TextNode('This is a text node', TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_missing_url_is_none(self):
+        node = TextNode('None URL', TextType.LINK)
+        self.assertIsNone(node.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
