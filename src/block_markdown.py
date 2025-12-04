b7693df77541e002e0dd792fcc3ebc02d0156377
e-zs <215135216+e-zs@users.noreply.github.com>
2025-12-04 18:47:36 +0100

    added block_to_block_types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,8 @@
 #python
 
+from enum import Enum
+import re
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split('\n\n')
     markdown_blocks = []
@@ -10,3 +13,39 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         markdown_blocks.append(block)
 
     return markdown_blocks
+
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
+def block_to_block_type(markdown_block: str) -> BlockType:
+    if re.match(r'^[#]{1,6}\s[^\n]+$', markdown_block):
+        return BlockType.HEADING
+    if re.match(r'(?s)^```.*```$', markdown_block):
+        return BlockType.CODE
+    if re.fullmatch(r'(?:>.*(?:\n|$))+', markdown_block):
+        return BlockType.QUOTE
+    if re.fullmatch(r'(?:- .*(?:\n|$))+', markdown_block):
+        return BlockType.UNORDERED_LIST
+    if is_ordered_numbered_list(markdown_block):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+
+
+def is_ordered_numbered_list(s: str) -> bool:
+    lines = s.splitlines()
+    if not lines:
+        return False
+    pat = re.compile(r'^(\d+)\.\s')
+    for i, line in enumerate(lines, start=1):
+        m = pat.match(line)
+        if not m:
+            return False
+        if int(m.group(1)) != i:
+            return False
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,11 @@
 #python
 
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    )
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_single_paragraph(self):
@@ -143,3 +147,115 @@ This is a line with **bold** and _italic_ and a `code block`
 
             ]
         )
+
+class TestBlockToBlock(unittest.TestCase):
+    def test_empty(self):
+        md = ''''''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_heading(self):
+        md = '# Level one heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '## Level two heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '### Level three heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '#### Level four heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '##### Level five heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '###### Level six heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+        md = '####### Level seven heading'
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+        md = '''# First level
+And a new line that is not a heading
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_code(self):
+        md = '''```This is a code block```'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.CODE)
+
+        md = '''```This is a code block
+with a new line
+
+and another new line
+```'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.CODE)
+
+        md = '''```This is a code block
+with a new line```
+
+and data after the code block
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_quote(self):
+        md = '''>A single quoted line'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.QUOTE)
+
+        md = '''>A single quoted line'
+>Followed by another'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.QUOTE)
+
+        md = '''>A single quoted line'
+Followed by a non-quoted line'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_unordered_list(self):
+        md = '''- Single list item'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+        md = '''- First list item
+- Second list item
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+        md = '''- First list item
+-Not a list item
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_ordered_list(self):
+        md = '''1. Only one list item'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+        md = '''1. First list item
+2. Second list item
+3. Third list item
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+        md = '''1. First list item
+3. Second list item out of order
+4. Third list item
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+        md = '''1. First list item
+2Not a list item
+'''
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
